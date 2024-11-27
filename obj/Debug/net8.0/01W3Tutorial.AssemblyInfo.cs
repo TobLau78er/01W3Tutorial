f9aa3d5e924d90e792bb1eb5086a8ac32b723aa4
tobias.lauer@outlook.com <tobias.lauer@outlook.com>
2024-11-27 17:55:56 +0100

    SecondCommit

--- obj/Debug/net8.0/01W3Tutorial.AssemblyInfo.cs
+++ obj/Debug/net8.0/01W3Tutorial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01W3Tutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccf05530737cfd6c5d3ffe261c76f525713c24dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73d3aaf48f98d8953c587ba75c6c1c9705411408")]
 [assembly: System.Reflection.AssemblyProductAttribute("01W3Tutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01W3Tutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
